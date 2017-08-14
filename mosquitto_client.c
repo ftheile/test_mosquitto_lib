233f65bee3b61d99c0cf7f749e30f7a4e140480a
Frank Theile <frank.theile@web.de>
2017-08-14 22:15:24 +0200

    Simple example of a MQTT client using the mosquitto library.

---
+++ mosquitto_client.c
@@ -0,0 +1,52 @@
+#include "mosquitto.h"
+#include <math.h>
+#include <stdio.h>
+#include <unistd.h>
+
+struct cli {
+	struct mosquitto* mosq;
+};
+
+static struct cli theClient;
+
+static void cli_init(struct cli* me)
+{
+	me->mosq = mosquitto_new(NULL, true, me);
+	mosquitto_connect(me->mosq, "localhost", 1883, 60);
+}
+
+static void cli_cleanup(struct cli* me)
+{
+	mosquitto_disconnect(me->mosq);
+	mosquitto_destroy(me->mosq);
+}
+
+static void cli_publish(struct cli* me, float temp)
+{
+	char payload[8];
+	int len;
+	len = sprintf(payload, "%.1f", temp);
+	mosquitto_publish(me->mosq, NULL, "sensors/temperature", len, payload, 1, false);
+	printf("%s\n", payload);
+}
+
+#define MAX_SAMPLES 20
+
+int main()
+{
+	int i;
+
+	mosquitto_lib_init();
+	cli_init(&theClient);
+
+	for (i=0; i<MAX_SAMPLES; i++) {
+		float temp = 10 * sin(i * 2 * M_PI / MAX_SAMPLES);
+		cli_publish(&theClient, temp);
+		sleep(1);
+	}
+
+	cli_cleanup(&theClient);
+	mosquitto_lib_cleanup();
+
+	return 0;
+}
